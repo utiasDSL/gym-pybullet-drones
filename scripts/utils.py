50e6f65574582c8b331b1c8e3615d6f0e6f04220
Jacopo Panerati <jacopo.panerati@robotics.utias.utoronto.ca>
2020-08-13 13:00:37 -0400

    Spaces for tabs

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,10 +4,10 @@ import time
 #### Sync the stepped simulation with the wall-clock ###############################################
 ####################################################################################################
 #### Arguments #####################################################################################
-#### - i (Integer)			    current step of the simulation #########################
-#### - start_time (Timestamp)		    timestamp of the simulation start ######################
-#### - timestep (Float)			    desired, wall-clock timestep to render the simulation ##
+#### - i (Integer)                          current step of the simulation #########################
+#### - start_time (Timestamp)               timestamp of the simulation start ######################
+#### - timestep (Float)                     desired, wall-clock timestep to render the simulation ##
 ####################################################################################################
 def sync(i, start_time, timestep):
-	elapsed = time.time()-start_time
-	if elapsed < i*(timestep): time.sleep(i*(timestep)-elapsed)	
+    elapsed = time.time()-start_time
+    if elapsed < i*(timestep): time.sleep(i*(timestep)-elapsed)
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_pybullet_drones',
-	version='0.1.0',
-	install_requires=['gym', 'pybullet', 'stable_baselines3']
+    version='0.1.0',
+    install_requires=['gym', 'pybullet', 'stable_baselines3']
 )
