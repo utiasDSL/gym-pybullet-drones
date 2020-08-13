8bb2782577ef143085bf46bbd626f53798be3cb6
JacopoPan <jacopo.panerati@polymtl.ca>
2020-08-13 10:06:51 -0400

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,9 +4,9 @@ import time
 #### Sync the stepped simulation with the wall-clock ###############################################
 ####################################################################################################
 #### Arguments #####################################################################################
-#### - i (Integer)							current step of the simulation #########################
-#### - start_time (Timestamp)				timestamp of the simulation start ######################
-#### - timestep (Float)						desired, wall-clock timestep to render the simulation ##
+#### - i (Integer)			    current step of the simulation #########################
+#### - start_time (Timestamp)		    timestamp of the simulation start ######################
+#### - timestep (Float)			    desired, wall-clock timestep to render the simulation ##
 ####################################################################################################
 def sync(i, start_time, timestep):
 	elapsed = time.time()-start_time
