9df71524781a15ddd0dc140fcfee7b6d20d1d840
Jacopo Panerati <jacopo.panerati@robotics.utias.utoronto.ca>
2020-08-13 22:14:51 -0400

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='gym_pybullet_drones',
     version='0.1.0',
-    install_requires=['gym', 'pybullet', 'stable_baselines3']
+    install_requires=['gym', 'pybullet', 'stable_baselines3', 'ray[rllib]']
 )
 
