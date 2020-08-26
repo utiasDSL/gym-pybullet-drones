a6f3985d7c6620d32d05297045dcaaabbdacbe7c
Jacopo Panerati <jacopo.panerati@robotics.utias.utoronto.ca>
2020-08-26 13:23:57 -0400

    Sporadic checks of the sync function

--- examples/utils.py
+++ examples/utils.py
@@ -9,6 +9,7 @@ import time
 #### - timestep (float)                     desired, wall-clock step of the simulation's rendering #
 ####################################################################################################
 def sync(i, start_time, timestep):
-    elapsed = time.time() - start_time
-    if elapsed<(i*timestep): time.sleep(timestep*i - elapsed)
+    if timestep>.04 or i%(int(1/(24*timestep)))==0:
+        elapsed = time.time() - start_time
+        if elapsed<(i*timestep): time.sleep(timestep*i - elapsed)
 
