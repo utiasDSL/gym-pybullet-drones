41e4e26043028b493022e14ba002e0b69f0f732f
Jacopo Panerati <jacopo.panerati@robotics.utias.utoronto.ca>
2020-09-12 10:23:55 -0400

    Version regression

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_pybullet_drones',
-    version='1.0.0',
+    version='0.3.0',
     install_requires=['gym', 'pybullet', 'stable_baselines3', 'ray[rllib]']
 )
