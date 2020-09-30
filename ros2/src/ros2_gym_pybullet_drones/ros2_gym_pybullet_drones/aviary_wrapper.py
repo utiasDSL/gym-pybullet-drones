df3ba255d1a33955032c071ef6d1e2978ec32067
Jacopo Panerati <jacopo.panerati@robotics.utias.utoronto.ca>
2020-09-30 15:27:10 -0400

    ROS2 Python example

--- ros2/src/ros2_gym_pybullet_drones/ros2_gym_pybullet_drones/aviary_wrapper.py
+++ ros2/src/ros2_gym_pybullet_drones/ros2_gym_pybullet_drones/aviary_wrapper.py
@@ -1,7 +1,7 @@
 import rclpy
 import numpy as np
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32MultiArray
 
 from gym_pybullet_drones.envs.BaseAviary import DroneModel, Physics
 from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
@@ -15,32 +15,39 @@ class AviaryWrapper(Node):
     #### WIP ###########################################################################################
     def __init__(self):
         super().__init__('aviary_wrapper')
-        #
-        self.env = CtrlAviary()
-        #
-        #### write observations 
-        self.publisher_ = self.create_publisher(String, 'obs', 10)
-        timer_freq_hz = 2; timer_period_sec = 1/timer_freq_hz
-        self.timer = self.create_timer(timer_period_sec, self.step_callback)
         self.i = 0
+        timer_freq_hz = 240
+        self.env = CtrlAviary(drone_model=DroneModel.CF2X, num_drones=1, neighbourhood_radius=np.inf, initial_xyzs=None, initial_rpys=None,
+                                physics=Physics.PYB, freq=timer_freq_hz, aggregate_phy_steps=1, gui=True, record=False, obstacles=False, user_debug_gui=True)
+        self.action = np.ones(4)*self.env.HOVER_RPM
+        #
+        #### step and write observations 
+        timer_period_sec = 1/timer_freq_hz
+        self.publisher_ = self.create_publisher(Float32MultiArray, 'obs', 10)
+        self.timer = self.create_timer(timer_period_sec, self.step_callback)
         #
         #### read actions
-        self.action_subscription = self.create_subscription(String, 'action', self.get_action_callback, 10)
+        self.action_subscription = self.create_subscription(Float32MultiArray, 'action', self.get_action_callback, 10)
         self.action_subscription  # prevent unused variable warning
 
     #### WIP ###########################################################################################
     def step_callback(self):
-        #
-        self.env.step({"0": np.array([0,0,0,0])})
-        #
-        msg = String(); msg.data = '(A ROS2 Python wrapper for gym-pybullet-drones: %d)' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing obs: "%s"' % msg.data)
         self.i += 1
+        obs, reward, done, info = self.env.step({"0": self.action})
+        #
+        msg = Float32MultiArray(); msg.data = obs["0"]["state"].tolist()
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing obs: "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f"' \
+                                                            % (msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4],
+                                                            msg.data[5], msg.data[6], msg.data[7], msg.data[8], msg.data[9],
+                                                            msg.data[10], msg.data[11], msg.data[12], msg.data[13], msg.data[14],
+                                                            msg.data[15], msg.data[16], msg.data[17], msg.data[18], msg.data[19]))
+        
 
     #### WIP ###########################################################################################
     def get_action_callback(self, msg):
-        self.get_logger().info('I receive action: "%s"' % msg.data)
+        self.get_logger().info('I received action: "%f" "%f" "%f" "%f"' % (msg.data[0], msg.data[1], msg.data[2], msg.data[3]))
+        self.action = np.array([msg.data[0], msg.data[1], msg.data[2], msg.data[3]])
 
 
 ######################################################################################################################################################
--- ros2/src/ros2_gym_pybullet_drones/ros2_gym_pybullet_drones/random_control.py
+++ ros2/src/ros2_gym_pybullet_drones/ros2_gym_pybullet_drones/random_control.py
@@ -1,6 +1,10 @@
 import rclpy
+import random
+import numpy as np
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32MultiArray
+
+from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
 
 ######################################################################################################################################################
 #### WIP #############################################################################################################################################
@@ -10,27 +14,36 @@ class RandomControl(Node):
     #### WIP ###########################################################################################
     def __init__(self):
         super().__init__('random_control')
+        self.i = 0
+        self.env = CtrlAviary()
         #
         #### read observations
-        self.obs_subscription = self.create_subscription(String, 'obs', self.get_obs_callback, 10)
+        self.obs_subscription = self.create_subscription(Float32MultiArray, 'obs', self.get_obs_callback, 10)
         self.obs_subscription  # prevent unused variable warning
         #
         #### write action 
-        self.publisher_ = self.create_publisher(String, 'action', 10)
-        timer_freq_hz = 2; timer_period_sec = 1/timer_freq_hz
+        self.publisher_ = self.create_publisher(Float32MultiArray, 'action', 10)
+        timer_freq_hz = 50; timer_period_sec = 1/timer_freq_hz
         self.timer = self.create_timer(timer_period_sec, self.action_callback)
-        self.i = 0
+        
 
     #### WIP ###########################################################################################
     def action_callback(self):
-        msg = String(); msg.data = 'Random action: %d)' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing action: "%s"' % msg.data)
         self.i += 1
+        #
+        random_rpm13 = random.gauss(self.env.HOVER_RPM+10, 500); random_rpm24 = random.gauss(self.env.HOVER_RPM+10, 500)
+        msg = Float32MultiArray(); msg.data = [random_rpm13,random_rpm24,random_rpm13,random_rpm24]
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing action: "%f" "%f" "%f" "%f"' % (msg.data[0], msg.data[1], msg.data[2], msg.data[3]))
+        
 
     #### WIP ###########################################################################################
     def get_obs_callback(self, msg):
-        self.get_logger().info('I observe: "%s"' % msg.data)
+        self.get_logger().info('I received obs: "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f" "%f"' \
+                                                            % (msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4],
+                                                            msg.data[5], msg.data[6], msg.data[7], msg.data[8], msg.data[9],
+                                                            msg.data[10], msg.data[11], msg.data[12], msg.data[13], msg.data[14],
+                                                            msg.data[15], msg.data[16], msg.data[17], msg.data[18], msg.data[19]))
 
 
 ######################################################################################################################################################
