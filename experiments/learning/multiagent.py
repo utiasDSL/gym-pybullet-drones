683600f995ef25f6f81528a6a6b87ed2211e7bbd
Jacopo Panerati <jacopo.panerati@robotics.utias.utoronto.ca>
2020-10-25 22:30:02 -0400

    Clean up of multi agent scripts

--- experiments/learning/multiagent.py
+++ experiments/learning/multiagent.py
@@ -35,35 +35,21 @@ from gym_pybullet_drones.envs.multi_agent_rl.MeetupAviary import MeetupAviary
 from gym_pybullet_drones.utils.Logger import Logger
 from gym_pybullet_drones.utils.utils import *
 
+######################################################################################################################################################
 class FillInActions(DefaultCallbacks):
     def on_postprocess_trajectory(self, worker, episode, agent_id, policy_id, policies, postprocessed_batch, original_batches, **kwargs):
         action_vec_size = 4
         to_update = postprocessed_batch[SampleBatch.CUR_OBS]
         other_id = 1 if agent_id == 0 else 0
         action_encoder = ModelCatalog.get_preprocessor_for_space( 
-                                                        Box(-np.inf, np.inf, (action_vec_size,), np.float32) # WHY? UNBOUNDED? # Discrete(2)
+                                                        Box(-np.inf, np.inf, (action_vec_size,), np.float32) # Unbounded
                                                         )
         _, opponent_batch = original_batches[other_id]
         opponent_actions = np.array([ action_encoder.transform(a) for a in opponent_batch[SampleBatch.ACTIONS] ])
         to_update[:, -action_vec_size:] = opponent_actions
 
-def central_critic_observer(agent_obs, **kw):
-    action_vec_size = 4
-    new_obs = {
-        0: {
-            "own_obs": agent_obs[0],
-            "opponent_obs": agent_obs[1],
-            "opponent_action": np.zeros(action_vec_size),  # filled in by FillInActions
-        },
-        1: {
-            "own_obs": agent_obs[1],
-            "opponent_obs": agent_obs[0],
-            "opponent_action": np.zeros(action_vec_size),  # filled in by FillInActions
-        },
-    }
-    return new_obs
-
-class YetAnotherTorchCentralizedCriticModel(TorchModelV2, nn.Module):
+######################################################################################################################################################
+class CustomTorchCentralizedCriticModel(TorchModelV2, nn.Module):
     """Multi-agent model that implements a centralized value function.
 
     It assumes the observation is a dict with 'own_obs' and 'opponent_obs', the
@@ -80,16 +66,15 @@ class YetAnotherTorchCentralizedCriticModel(TorchModelV2, nn.Module):
         TorchModelV2.__init__(self, obs_space, action_space, num_outputs, model_config, name)
         nn.Module.__init__(self)
         self.action_model = FullyConnectedNetwork(
-            #Box(low=0, high=1, shape=(6, )),  # one-hot encoded Discrete(6)
-            Box(low=-1, high=1, shape=(20, )),  # one-hot encoded Discrete(6)
-            Box(low=-1, high=1, shape=(4, )), # action_space,
+            Box(low=-1, high=1, shape=(20, )), # DOUBLE CHECK / MODIFY THIS # Box(low=0, high=1, shape=(6, )),  # one-hot encoded Discrete(6)
+            action_space,
             num_outputs,
             model_config,
             name + "_action"
             )
         self.value_model = FullyConnectedNetwork(
             obs_space, 
-            Box(low=-1, high=1, shape=(4, )), # action_space,
+            action_space,
             1, 
             model_config, 
             name + "_vf"
@@ -97,7 +82,6 @@ class YetAnotherTorchCentralizedCriticModel(TorchModelV2, nn.Module):
         self._model_in = None
 
     def forward(self, input_dict, state, seq_lens):
-        # Store model-input for possible `value_function()` call.
         self._model_in = [input_dict["obs_flat"], state, seq_lens]
         return self.action_model({ "obs": input_dict["obs"]["own_obs"] }, state, seq_lens)
 
@@ -105,98 +89,91 @@ class YetAnotherTorchCentralizedCriticModel(TorchModelV2, nn.Module):
         value_out, _ = self.value_model({ "obs": self._model_in[0] }, self._model_in[1], self._model_in[2])
         return torch.reshape(value_out, [-1])
 
+######################################################################################################################################################
+def central_critic_observer(agent_obs, **kw):
+    action_vec_size = 4
+    new_obs = {
+        0: {
+            "own_obs": agent_obs[0],
+            "opponent_obs": agent_obs[1],
+            "opponent_action": np.zeros(action_vec_size),  # Filled in by FillInActions
+        },
+        1: {
+            "own_obs": agent_obs[1],
+            "opponent_obs": agent_obs[0],
+            "opponent_action": np.zeros(action_vec_size),  # Filled in by FillInActions
+        },
+    }
+    return new_obs
 
-
-
+######################################################################################################################################################
 if __name__ == "__main__":
 
     #### Define and parse (optional) arguments for the script ##########################################
     parser = argparse.ArgumentParser(description='Multi-agent reinforcement learning experiments script')
     parser.add_argument('--num_drones', default=2,         type=int,                                                           help='Number of drones (default: 2)', metavar='')
     parser.add_argument('--env',        default='flock',   type=str,       choices=['leaderfollower', 'flock', 'meetup'],      help='Help (default: ..)', metavar='')
-    parser.add_argument('--algo',       default='cc',      type=str,       choices=['cc', 'other'],                            help='Help (default: ..)', metavar='')
     ARGS = parser.parse_args()
-    filename = os.path.dirname(os.path.abspath(__file__))+'/save-'+ARGS.env+'-'+ARGS.algo+'-'+datetime.now().strftime("%m.%d.%Y_%H.%M.%S")
+    filename = os.path.dirname(os.path.abspath(__file__))+'/save-'+ARGS.env+'-'+datetime.now().strftime("%m.%d.%Y_%H.%M.%S")
 
     #### Initialize Ray Tune ###########################################################################
     ray.shutdown()
     ray.init(ignore_reinit_error=True)
 
-    if ARGS.algo=='cc':
 
-        ModelCatalog.register_custom_model(
-            "cc_model", YetAnotherTorchCentralizedCriticModel #if args.torch else YetAnotherCentralizedCriticModel
-        )
+    #### Register the custom centralized critic model ##################################################
+    ModelCatalog.register_custom_model( "cc_model", CustomTorchCentralizedCriticModel )
 
-        #### Unused env to extract correctly sized action and observation spaces ###########################
-        unused_env = FlockAviary(num_drones=ARGS.num_drones)
-        # print(unused_env.action_space["0"]) # Box(-1.0, 1.0, (4,), float32)
-        # print(unused_env.observation_space["0"]) # Dict(neighbors:MultiBinary(2), state:Box(-1.0, 1.0, (20,), float32))
-        action_space = unused_env.action_space[0]
-        observer_space = Dict({
-            "own_obs": unused_env.observation_space[0],
-            "opponent_obs": unused_env.observation_space[0],
-            "opponent_action": unused_env.action_space[0],
-        })
+    #### Unused env to extract correctly sized action and observation spaces ###########################
+    temp_env = FlockAviary(num_drones=ARGS.num_drones)
+    observer_space = Dict({
+        "own_obs": temp_env.observation_space[0], # Box(-1.0, 1.0, (20,), np.float32) or Dict(neighbors:MultiBinary(2), state:Box(-1.0, 1.0, (20,), np.float32))
+        "opponent_obs": temp_env.observation_space[0],
+        "opponent_action": temp_env.action_space[0],
+    })
+    action_space = temp_env.action_space[0] # Box(-1.0, 1.0, (4,), np.float32)
 
-        #### Register the environment ######################################################################
-        register_env("this-flock-aviary-v0", lambda _: FlockAviary(num_drones=ARGS.num_drones))
+    #### Register the environment ######################################################################
+    register_env("this-flock-aviary-v0", lambda _: FlockAviary(num_drones=ARGS.num_drones))
 
-        #### Set up the trainer's config ###################################################################
-        config = ppo.DEFAULT_CONFIG.copy() # for the default config, see github.com/ray-project/ray/blob/master/rllib/agents/trainer.py
-        
-        config = {
-            "env": "this-flock-aviary-v0",
-            "batch_mode": "complete_episodes",
-            "callbacks": FillInActions,
-            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-            # "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
-            "num_workers": 0,
-            "model": {
-               "custom_model": "cc_model",
-            },
-            "framework": "torch", # if args.torch else "tf",
-        }
-        
-        config["multiagent"] = { 
-            # Map of type MultiAgentPolicyConfigDict from policy ids to tuples of (policy_cls, obs_space, act_space, config).
-            # This defines the observation and action spaces of the policies and any extra config.
-            "policies": {
-                # "pol0": (PPOTFPolicy, unused_env.observation_space["0"], unused_env.action_space["0"], {"framework": "torch"}),
-                "pol0": (None, observer_space, action_space, {"agent_id": 0,}),
-                "pol1": (None, observer_space, action_space, {"agent_id": 1,}),
-            },
-            # Function mapping agent ids to policy ids.
-            #"policy_mapping_fn": lambda agent_id: "pol"+str(agent_id),
-            "policy_mapping_fn": lambda x: "pol0" if x == 0 else "pol1",
-            # An additional observation function, see rllib/evaluation/observation_function.py for more info.
-            "observation_fn": central_critic_observer, # "observation_fn": None,
-            # see github.com/ray-project/ray/blob/master/rllib/examples/centralized_critic_2.py
-            # more principled but complex way to share observations is using `postprocess_trajectory`
-            # see github.com/ray-project/ray/blob/master/rllib/examples/centralized_critic.py
-        }
+    #### Set up the trainer's config ###################################################################
+    config = ppo.DEFAULT_CONFIG.copy() # For the default config, see github.com/ray-project/ray/blob/master/rllib/agents/trainer.py
+    config = {
+        "env": "this-flock-aviary-v0",
+        "batch_mode": "complete_episodes",
+        "callbacks": FillInActions,
+        # "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")), # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0
+        "num_workers": 0,
+        "model": {
+           "custom_model": "cc_model",
+        },
+        "framework": "torch",
+    }
+    
+    #### Set up the multiagent parameters of the trainer's config ######################################
+    config["multiagent"] = { 
+        "policies": {
+            "pol0": (None, observer_space, action_space, {"agent_id": 0,}),
+            "pol1": (None, observer_space, action_space, {"agent_id": 1,}),
+        },
+        "policy_mapping_fn": lambda x: "pol0" if x == 0 else "pol1", # # Function mapping agent ids to policy ids
+        "observation_fn": central_critic_observer, # See rllib/evaluation/observation_function.py for more info
+    }
 
-        #### Ray Tune stopping conditions ##################################################################
-        stop = {
-            "timesteps_total": 10, #8000
-            # "episode_reward_mean": 0,
-        }
+    #### Ray Tune stopping conditions ##################################################################
+    stop = {
+        "timesteps_total": 10, # 8000,
+        # "episode_reward_mean": 0,
+    }
 
-        #### Train #########################################################################################
-        results = tune.run(
-            "PPO",
-            stop=stop,
-            config=config,
-            verbose=True,
-            checkpoint_at_end=True
-        ) 
-
-    elif ARGS.algo=='other':
-        print("Not yet implemented")
-        pass
-
-
-
+    #### Train #########################################################################################
+    results = tune.run(
+        "PPO",
+        stop=stop,
+        config=config,
+        verbose=True,
+        checkpoint_at_end=True
+    )
     # check_learning_achieved(results, 1.0)
 
     #### Save agent #################################################################################
@@ -204,6 +181,7 @@ if __name__ == "__main__":
     checkpoint_path = checkpoints[0][0]
     print(checkpoint_path)
 
+    #### Shut down Ray #################################################################################
     ray.shutdown()
 
 
--- experiments/learning/test_multiagent.py
+++ experiments/learning/test_multiagent.py
@@ -19,14 +19,10 @@ from ray.rllib.agents.ppo import PPOTrainer, PPOTFPolicy
 from ray.rllib.examples.policy.random_policy import RandomPolicy
 from ray.rllib.utils.test_utils import check_learning_achieved
 
-
 from gym_pybullet_drones.envs.BaseAviary import DroneModel, Physics
-
 from gym_pybullet_drones.envs.multi_agent_rl.FlockAviary import FlockAviary
 from gym_pybullet_drones.envs.multi_agent_rl.LeaderFollowerAviary import LeaderFollowerAviary
 from gym_pybullet_drones.envs.multi_agent_rl.MeetupAviary import MeetupAviary
-
-
 from gym_pybullet_drones.utils.Logger import Logger
 from gym_pybullet_drones.utils.utils import *
 
@@ -40,62 +36,49 @@ if __name__ == "__main__":
     parser.add_argument('--file',                           type=str,       help='Help (default: ..)', metavar='')
     ARGS = parser.parse_args()
 
-    # /Users/jacopo/ray_results/PPO/PPO_this-flock-aviary-v0_136de_00000_0_2020-10-25_20-20-39/checkpoint_1/checkpoint-1
-
+    # e.g. python test_multiagent.py --file /Users/jacopo/ray_results/PPO/PPO_this-flock-aviary-v0_136de_00000_0_2020-10-25_20-20-39/checkpoint_1/checkpoint-1
 
     #### Initialize Ray Tune ###########################################################################
     ray.shutdown()
     ray.init(ignore_reinit_error=True)
 
-    ModelCatalog.register_custom_model(
-        "cc_model", YetAnotherTorchCentralizedCriticModel #if args.torch else YetAnotherCentralizedCriticModel
-    )
+    #### Register the custom centralized critic model ##################################################
+    ModelCatalog.register_custom_model( "cc_model", CustomTorchCentralizedCriticModel )
 
     #### Unused env to extract correctly sized action and observation spaces ###########################
-    unused_env = FlockAviary(num_drones=ARGS.num_drones)
-    # print(unused_env.action_space["0"]) # Box(-1.0, 1.0, (4,), float32)
-    # print(unused_env.observation_space["0"]) # Dict(neighbors:MultiBinary(2), state:Box(-1.0, 1.0, (20,), float32))
-    action_space = unused_env.action_space[0]
+    temp_env = FlockAviary(num_drones=ARGS.num_drones)
     observer_space = Dict({
-        "own_obs": unused_env.observation_space[0],
-        "opponent_obs": unused_env.observation_space[0],
-        "opponent_action": unused_env.action_space[0],
+        "own_obs": temp_env.observation_space[0], # Box(-1.0, 1.0, (20,), np.float32) or Dict(neighbors:MultiBinary(2), state:Box(-1.0, 1.0, (20,), np.float32))
+        "opponent_obs": temp_env.observation_space[0],
+        "opponent_action": temp_env.action_space[0],
     })
+    action_space = temp_env.action_space[0] # Box(-1.0, 1.0, (4,), np.float32)
 
     #### Register the environment ######################################################################
     register_env("this-flock-aviary-v0", lambda _: FlockAviary(num_drones=ARGS.num_drones))
 
-    config = ppo.DEFAULT_CONFIG.copy() # for the default config, see github.com/ray-project/ray/blob/master/rllib/agents/trainer.py
-        
+    #### Set up the trainer's config ###################################################################
+    config = ppo.DEFAULT_CONFIG.copy() # For the default config, see github.com/ray-project/ray/blob/master/rllib/agents/trainer.py
     config = {
         "env": "this-flock-aviary-v0",
         "batch_mode": "complete_episodes",
         "callbacks": FillInActions,
-        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-        # "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+        # "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")), # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0
         "num_workers": 0,
         "model": {
            "custom_model": "cc_model",
         },
-        "framework": "torch", # if args.torch else "tf",
+        "framework": "torch",
     }
     
+    #### Set up the multiagent parameters of the trainer's config ######################################
     config["multiagent"] = { 
-        # Map of type MultiAgentPolicyConfigDict from policy ids to tuples of (policy_cls, obs_space, act_space, config).
-        # This defines the observation and action spaces of the policies and any extra config.
         "policies": {
-            # "pol0": (PPOTFPolicy, unused_env.observation_space["0"], unused_env.action_space["0"], {"framework": "torch"}),
             "pol0": (None, observer_space, action_space, {"agent_id": 0,}),
             "pol1": (None, observer_space, action_space, {"agent_id": 1,}),
         },
-        # Function mapping agent ids to policy ids.
-        #"policy_mapping_fn": lambda agent_id: "pol"+str(agent_id),
-        "policy_mapping_fn": lambda x: "pol0" if x == 0 else "pol1",
-        # An additional observation function, see rllib/evaluation/observation_function.py for more info.
-        "observation_fn": central_critic_observer, # "observation_fn": None,
-        # see github.com/ray-project/ray/blob/master/rllib/examples/centralized_critic_2.py
-        # more principled but complex way to share observations is using `postprocess_trajectory`
-        # see github.com/ray-project/ray/blob/master/rllib/examples/centralized_critic.py
+        "policy_mapping_fn": lambda x: "pol0" if x == 0 else "pol1", # # Function mapping agent ids to policy ids
+        "observation_fn": central_critic_observer, # See rllib/evaluation/observation_function.py for more info
     }
 
     #### Restore agent #################################################################################
@@ -105,33 +88,29 @@ if __name__ == "__main__":
     #### Extract and print policies ####################################################################
     policy0 = agent.get_policy("pol0")
     policy1 = agent.get_policy("pol1")
-    #print(policy0.model.base_model.summary())
-    #print(policy1.model.base_model.summary())
+    print(policy0.model.action_model)
+    print(policy0.model.value_model)
+    print(policy1.model.action_model)
+    print(policy1.model.value_model)
 
     #### Create test environment ########################################################################
     env = FlockAviary(num_drones=ARGS.num_drones, gui=True, record=False, obstacles=True)
     obs = env.reset()
-    # action = { str(i): np.array([0,0,0,0]) for i in range(ARGS.num_drones) }
-    action = { i: np.array([0,0,0,0]) for i in range(ARGS.num_drones) }
+    action = {    i   : np.array([0,0,0,0]) for i in range(ARGS.num_drones) }
     start = time.time()
     for i in range(10*env.SIM_FREQ):
 
         #### Deploy the policies ###########################################################################
-        print("Debug Obs", obs)
+        # print("Debug Obs", obs)
         temp = {}
-        # temp["0"] = policy0.compute_single_action(np.hstack([ obs["0"]["state"], obs["0"]["neighbors"] ]))
-        # temp["1"] = policy1.compute_single_action(np.hstack([ obs["1"]["state"], obs["1"]["neighbors"] ]))
-        # print("Debug Act", temp)
-        # action = {"0": temp["0"][0], "1": temp["1"][0], "2": temp["2"][0]}
         temp[0] = policy0.compute_single_action(np.hstack([ obs[0], obs[1], action[1] ]))
         temp[1] = policy1.compute_single_action(np.hstack([ obs[1], obs[0], action[0] ]))
-        print("Debug Act", temp)
+        # print("Debug Act", temp)
         action = {0: temp[0][0], 1: temp[1][0]}
-
         obs, reward, done, info = env.step(action)
         env.render()
         sync(i, start, env.TIMESTEP)
-        # if done["__all__"]: obs = env.reset()
+        if done["__all__"]: obs = env.reset()
     env.close()
 
     #### Shut down Ray #################################################################################
