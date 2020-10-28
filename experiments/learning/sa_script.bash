fc598d5732f10c6a1feaeb52297c4684f495073e
Jacopo Panerati <jacopo.panerati@robotics.utias.utoronto.ca>
2020-10-28 15:51:15 -0400

    Slurm scripts

--- experiments/learning/sa_script.bash
+++ experiments/learning/sa_script.bash
@@ -2,11 +2,14 @@
 
 # for env in ['takeoff', 'hover', 'flythrugate']; do
 #     for algo in ['a2c', 'ppo', 'sac', 'td3', 'ddpg']; do
-for env in ['takeoff', 'hover']; do
-    for algo in ['a2c', 'ppo']; do
-        for pol in ['mlp', 'cnn']; do
-            for input in ['rpm', 'dyn']; do
-                sbatch sa_script.slrm ${env} ${algo} ${pol} ${input}
+#         for pol in ['mlp', 'cnn']; do
+#             for input in ['rpm', 'dyn']; do
+for env in ['takeoff']; do
+    for algo in ['a2c']; do
+        for pol in ['mlp']; do
+            for input in ['rpm']; do
+                # sbatch sa_script.slrm ${env} ${algo} ${pol} ${input}
+                sbatch --export=env=${env},algo=${algo},pol=${pol},input=${input} sa_script.slrm
                 # bash launch_slurm_job.sh gpu example_job_${dropout}_${lr} 1 "python3 train.py --dropout ${dropout} --lr ${lr}"
             done
         done
