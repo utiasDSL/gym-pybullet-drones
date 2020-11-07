8cc42d6b962e55edd7a6a04f97625c03b8f75410
Jacopo Panerati <jacopo.panerati@robotics.utias.utoronto.ca>
2020-11-07 11:53:35 -0500

    Assignments docstrings

--- assignments/hw1_control.py
+++ assignments/hw1_control.py
@@ -1,71 +1,108 @@
-import math
-import numpy as np
-from scipy.spatial.transform import Rotation
+"""Control implementation for assignment 1.
+The script is used the simulation in file "hw_simulation.py".
 
+Example:
+    To run the simulation, type in a terminal
+
+        $ python hw1_simulation.py
+
+Todo:
+    * Implement method HW1Control.compute_control()
+    * Tune the PID coefficients in HW1Control.__init__()
+    * ..
+"""
+import numpy as np
 from gym_pybullet_drones.envs.BaseAviary import BaseAviary
 
-class HW1Control(object):
+class HW1Control():
+    """Control class for assignment 1.
+    """
 
-    #### Initialize the controller #####################################################################
-    def __init__(self, 
-                    env: BaseAviary
-                    ):
-        #### Set general use constants #####################################################################
-        self.GRAVITY = env.GRAVITY # i.e., M * g
-        self.TIMESTEP = env.TIMESTEP
-        self.KF = env.KF
-        self.KM = env.KM
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        self.P_COEFF_POSITION = np.array([1, 1, 1]) # HOMEWORK CODE
-        self.I_COEFF_POSITION = np.array([1, 1, 1]) # HOMEWORK CODE
-        self.D_COEFF_POSITION = np.array([1, 1, 1]) # HOMEWORK CODE
-        self.P_COEFF_ATTITUDE = np.array([1, 1, 1]) # HOMEWORK CODE
-        self.I_COEFF_ATTITUDE = np.array([1, 1, 1]) # HOMEWORK CODE
-        self.D_COEFF_ATTITUDE = np.array([1, 1, 1]) # HOMEWORK CODE
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
+    def __init__(self,
+                 env: BaseAviary
+                 ):
+        """ Initialization of class HW1Control.
+
+        Args:
+            env (BaseAviary): the PyBullet-based simulation environment.
+        """
+        self.gravity = env.GRAVITY
+        """float: gravity, the product of the drone's mass M by acc g."""
+        self.timestep = env.TIMESTEP
+        """float: simulation and control timestep."""
+        self.kf_coeff = env.KF
+        """float: RPMs to force coefficient."""
+        self.km_coeff = env.KM
+        """float: RPMs to torque coefficient."""
+        ############################################################
+        ############################################################
+        #### HOMEWORK CODE (START) #################################
+        ############################################################
+        ############################################################
+        self.p_coeff_position = np.array([1, 1, 1])
+        self.i_coeff_position = np.array([1, 1, 1])
+        self.d_coeff_position = np.array([1, 1, 1])
+        self.p_coeff_attitude = np.array([1, 1, 1])
+        self.i_coeff_attitude = np.array([1, 1, 1])
+        self.d_coeff_attitude = np.array([1, 1, 1])
+        ############################################################
+        ############################################################
+        #### HOMEWORK CODE (END) ###################################
+        ############################################################
+        ############################################################
         self.reset()
 
-    #### Reset the controller ##########################################################################
     def reset(self):
+        """ Resets the controller counters and variables (integral errors and
+        previous control step errors).
+        """
         self.control_counter = 0
         self.last_position_error = np.zeros(3)
         self.integral_position_error = np.zeros(3)
         self.last_attitude_error = np.zeros(3)
         self.integral_attitude_error = np.zeros(3)
 
-    #### Compute the control action ####################################################################
-    def computeControl(self, 
-                        current_position, 
-                        current_quaternion, 
-                        current_velocity, 
+    def compute_control(self,
+                        current_position,
+                        current_quaternion,
+                        current_velocity,
                         current_angular_velocity,
-                        target_position, 
+                        target_position,
                         target_velocity=np.zeros(3),
-                        target_angular_velocity=np.zeros(3)
                         ):
+        """Compute the propellers' RPMs for the target state, given the
+        current state.
+
+        Args:
+            current_position ((3,) NumPy array): global x, y, z, in meters.
+            current_quaternion ((4,) NumPy array): global, quaternion.
+            current_velocity ((3,) NumPy array): global vx, vy, vz, in m/s.
+            current_angular_velocity ((3,) NumPy array): global, in rad/s.
+            target_position ((3,) NumPy array): global x, y, z, in meters.
+            target_velocity ((3,) NumPy array, optional): global, in m/s.
+
+        Returns:
+            (4,) NumPy array with the desired RPMs of each propeller.
+        """
         self.control_counter += 1
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        propellers_0_and_3_rpm = np.sqrt(self.GRAVITY/(4*self.KF)) # HOMEWORK CODE
-        propellers_1_and_2_rpm = np.sqrt(self.GRAVITY/(4*self.KF)) # HOMEWORK CODE
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        ####################################################################################################
-        return np.array([propellers_0_and_3_rpm, propellers_1_and_2_rpm, propellers_1_and_2_rpm, propellers_0_and_3_rpm])
-
-
-
-
+        ############################################################
+        ############################################################
+        #### HOMEWORK CODE (START) #################################
+        ############################################################
+        ############################################################
+        if self.control_counter%(1/self.timestep) == 0:
+            print(current_position)
+            print(current_quaternion)
+            print(current_velocity)
+            print(current_angular_velocity)
+            print(target_position)
+            print(target_velocity)
+        propellers_0_and_3_rpm = np.sqrt(self.gravity/(4*self.kf_coeff))
+        propellers_1_and_2_rpm = np.sqrt(self.gravity/(4*self.kf_coeff))
+        ############################################################
+        ############################################################
+        #### HOMEWORK CODE (END) ###################################
+        ############################################################
+        ############################################################
+        return np.array([propellers_0_and_3_rpm, propellers_1_and_2_rpm,
+                         propellers_1_and_2_rpm, propellers_0_and_3_rpm])
--- assignments/hw1_simulation.py
+++ assignments/hw1_simulation.py
@@ -1,93 +1,79 @@
-import os
-import time
-import argparse
-from datetime import datetime
-import pdb
-import math
-import random
-import numpy as np
-import pybullet as p
-import matplotlib.pyplot as plt
+"""Simulation script for assignment 1.
+The script uses the control defined in file "hw_control.py".
 
+Example:
+    To run the simulation, type in a terminal
+
+        $ python hw1_simulation.py
+
+"""
+import time
+import numpy as np
 from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
 from gym_pybullet_drones.utils.Logger import Logger
-from gym_pybullet_drones.utils.utils import *
-
+from gym_pybullet_drones.utils.utils import sync
 from hw1_control import HW1Control
-# from gym_pybullet_drones.control.DSLPIDControl import DSLPIDControl
 
-DURATION = 10
+DURATION = 10 # int: the duration of the simulation in seconds
+GUI = True # bool: whether to use PyBullet graphical interface
 
 if __name__ == "__main__":
 
-    #### Create the environment ########################################################################
-    GUI = True # set to False to disable rendering
-    env = CtrlAviary(gui=GUI)  
+    # Create the ENVironment ###################################
+    ENV = CtrlAviary(gui=GUI)
 
-    #### Initialize the logger #########################################################################
-    logger = Logger(logging_freq_hz=env.SIM_FREQ)
+    # Initialize the LOGGER ####################################
+    LOGGER = Logger(logging_freq_hz=ENV.SIM_FREQ)
 
-    #### Initialize the controller #####################################################################
-    ctrl = HW1Control(env)
-    # ctrl2 = DSLPIDControl(env)
+    # Initialize the controller ################################
+    CTRL = HW1Control(ENV)
 
-    #### Initialize the action #########################################################################
-    action = {}
-    obs = env.reset()
-    state = obs["0"]["state"]
-    action["0"] = ctrl.computeControl(current_position=state[0:3], 
-                                        current_quaternion=state[3:7], 
-                                        current_velocity=state[10:13], 
-                                        current_angular_velocity=state[13:16],
-                                        target_position=state[0:3], 
-                                        target_velocity=np.zeros(3)
-                                        )
-    # action["0"], _, _ = ctrl2.computeControlFromState(control_timestep=env.TIMESTEP, state=state, target_pos=state[0:3])
+    # Initialize the ACTION ####################################
+    ACTION = {}
+    OBS = ENV.reset()
+    STATE = OBS["0"]["state"]
+    ACTION["0"] = CTRL.compute_control(current_position=STATE[0:3],
+                                       current_quaternion=STATE[3:7],
+                                       current_velocity=STATE[10:13],
+                                       current_angular_velocity=STATE[13:16],
+                                       target_position=STATE[0:3],
+                                       target_velocity=np.zeros(3)
+                                       )
 
-    #### Initialize target trajectory ##################################################################
-    target_trajectory = np.array([ [np.sin(i*((2*np.pi)/(DURATION*env.SIM_FREQ))), 0, state[2]] for i in range(DURATION*env.SIM_FREQ) ])
-    target_velocity = np.zeros([DURATION*env.SIM_FREQ,3])
+    # Initialize target trajectory #############################
+    TARGET_TRAJECTORY = np.array([[np.sin(i*((2*np.pi)/(DURATION*ENV.SIM_FREQ))), 0, STATE[2]] for i in range(DURATION*ENV.SIM_FREQ)])
+    TARGET_VELOCITY = np.zeros([DURATION*ENV.SIM_FREQ, 3])
 
-    #### Run the simulation ############################################################################
-    
+    # Run the simulation #######################################
     START = time.time()
-    for i in range(0, DURATION*env.SIM_FREQ):
+    for i in range(0, DURATION*ENV.SIM_FREQ):
 
-        #### Step the simulation ###########################################################################
-        obs, _, _, _ = env.step(action)
+        # Step the simulation ######################################
+        OBS, _, _, _ = ENV.step(ACTION)
 
-        #### Compute control ###############################################################################
-        state = obs["0"]["state"]
-        action["0"] = ctrl.computeControl(current_position=state[0:3], 
-                                            current_quaternion=state[3:7], 
-                                            current_velocity=state[10:13], 
-                                            current_angular_velocity=state[13:16],
-                                            target_position=target_trajectory[i,:], 
-                                            target_velocity=target_velocity[i,:]
-                                            )
-        # action["0"], _, _ = ctrl2.computeControlFromState(control_timestep=env.TIMESTEP, state=state, target_pos=target_trajectory[i,:])
+        # Compute control ##########################################
+        STATE = OBS["0"]["state"]
+        ACTION["0"] = CTRL.compute_control(current_position=STATE[0:3],
+                                           current_quaternion=STATE[3:7],
+                                           current_velocity=STATE[10:13],
+                                           current_angular_velocity=STATE[13:16],
+                                           target_position=TARGET_TRAJECTORY[i, :],
+                                           target_velocity=TARGET_VELOCITY[i, :]
+                                           )
 
-        #### Log the simulation ############################################################################
-        logger.log(drone=0, timestamp=i/env.SIM_FREQ, state=obs["0"]["state"])
+        # Log the simulation #######################################
+        LOGGER.log(drone=0, timestamp=i/ENV.SIM_FREQ, state=STATE)
 
-        #### Printout ######################################################################################
-        if i%env.SIM_FREQ==0: env.render()
+        # Printout #################################################
+        if i%ENV.SIM_FREQ == 0:
+            ENV.render()
 
-        #### Sync the simulation ###########################################################################
-        if GUI: sync(i, START, env.TIMESTEP) 
+        #### Sync the simulation ###################################
+        if GUI:
+            sync(i, START, ENV.TIMESTEP)
 
-    #### Close the environment #########################################################################
-    env.close()
+    # Close the ENVironment ####################################
+    ENV.close()
 
-    #### Plot the simulation results ###################################################################
-    logger.plot()
-
-
-
-
-
-
-
-
-
-
+    # Plot the simulation results ##############################
+    LOGGER.plot()
