c53b703a852209fc1015fc9cea49e7cc7a3cb469
Jacopo Panerati <jacopo.panerati@robotics.utias.utoronto.ca>
2020-12-06 14:13:41 -0500

    Temporary fix for PID control within RL aviaries

--- gym_pybullet_drones/envs/single_agent_rl/BaseSingleAgentAviary.py
+++ gym_pybullet_drones/envs/single_agent_rl/BaseSingleAgentAviary.py
@@ -11,6 +11,8 @@ from gym_pybullet_drones.utils.utils import nnlsRPM
 from gym_pybullet_drones.control.DSLPIDControl import DSLPIDControl
 from gym_pybullet_drones.control.SimplePIDControl import SimplePIDControl
 
+from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
+
 class ActionType(Enum):
     """Action type enumeration class."""
     RPM = "rpm"                 # RPMS
@@ -85,10 +87,10 @@ class BaseSingleAgentAviary(BaseAviary):
         #### Create integrated controllers #########################
         if act in [ActionType.PID, ActionType.ONE_D_PID]:
             os.environ['KMP_DUPLICATE_LIB_OK']='True'
-            if self.DRONE_MODEL in [DroneModel.CF2X, DroneModel.CF2P]:
-                self.ctrl = [DSLPIDControl(BaseAviary(drone_model=DroneModel.CF2X)) for i in range(self.NUM_DRONES)]
-            elif self.DRONE_MODEL == DroneModel.HB:
-                self.ctrl = [SimplePIDControl(BaseAviary(drone_model=DroneModel.HB)) for i in range(self.NUM_DRONES)]
+            if drone_model in [DroneModel.CF2X, DroneModel.CF2P]:
+                self.ctrl = [DSLPIDControl(CtrlAviary(drone_model=DroneModel.CF2X))]
+            elif drone_model == DroneModel.HB:
+                self.ctrl = [SimplePIDControl(CtrlAviary(drone_model=DroneModel.HB))]
         super().__init__(drone_model=drone_model,
                          num_drones=1,
                          initial_xyzs=initial_xyzs,
