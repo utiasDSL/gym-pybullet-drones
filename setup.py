d05e6984f25c47f589c81d03250972eb37d39b89
Jacopo Panerati <jacopo.panerati@robotics.utias.utoronto.ca>
2020-12-14 19:53:09 -0500

    Transfer to utiasDSL

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='gym_pybullet_drones',
-    version='0.5.2',
+    version='0.6.0',
     install_requires=['numpy', 'Pillow', 'matplotlib', 'cycler', 'gym', 'pybullet', 'stable_baselines3', 'ray[rllib]']
 )
