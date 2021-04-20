3634ba2eab4cadac040ab99b64b8e5023dd2151b
Jonathan Gill <jtgill@email.arizona.edu>
2021-04-20 16:22:26 -0700

    Added drone URDF to the environment.

--- tests/test_add_ball.py
+++ tests/test_add_ball.py
@@ -3,6 +3,8 @@
 import pybullet as pb
 import time
 import pybullet_data
+import os
+
 physicsClient = pb.connect(pb.GUI)#or pb.DIRECT for non-graphical version
 pb.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 pb.setGravity(0,0,-10)
@@ -12,11 +14,19 @@ startOrientation = pb.getQuaternionFromEuler([0,0,0])
 sphere_id = pb.loadURDF("sphere2.urdf",
     [0, 2, .5],
     pb.getQuaternionFromEuler([0,0,0]),
+    globalScaling=0.0625,
     physicsClientId=physicsClient
 )
 pb.changeDynamics(sphere_id, -1, mass=0.1)
 print("sphere id:", sphere_id)
 first_time = True
+drone_urdf = "cf2x.urdf"
+pb.loadURDF(
+    os.path.dirname(os.path.abspath(__file__))+"/../gym_pybullet_drones/assets/" + drone_urdf,
+    [0, 0, 0],
+    pb.getQuaternionFromEuler([0, 0, 0]),
+    physicsClientId=physicsClient
+)
 #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
 for i in range (10000):
     if first_time:
