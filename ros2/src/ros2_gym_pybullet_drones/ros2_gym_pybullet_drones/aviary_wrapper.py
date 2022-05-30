00fba45ffa7eefa2597054af3dee2fcf2c045d3b
Beomyeol Yu <yubeomyeol@gmail.com>
2022-05-30 14:16:20 -0400

    getpass.getuser() instead of os.getlogin() for Ubuntu/ROS2

--- ros2/src/ros2_gym_pybullet_drones/ros2_gym_pybullet_drones/aviary_wrapper.py
+++ ros2/src/ros2_gym_pybullet_drones/ros2_gym_pybullet_drones/aviary_wrapper.py
@@ -4,9 +4,12 @@ It creates an environment CtrlAviary and continually calls CtrlAviary.step().
 It publishes on topic 'obs' and reads from topic 'action'.
 """
 import sys, os  # See: https://github.com/utiasDSL/gym-pybullet-drones/issues/89
+import getpass
 sys.path.append(sys.path[0].replace("ros2/install/ros2_gym_pybullet_drones/lib/ros2_gym_pybullet_drones", ""))
-sys.path.append("/Users/"+os.getlogin()+"/opt/anaconda3/envs/drones/lib/python3.8/site-packages")  # macOS
-# sys.path.append("/home/"+os.getlogin()+"/anaconda3/envs/drones/lib/python3.8/site-packages")  # Ubuntu
+if sys.platform == 'darwin': # macOS
+    sys.path.append("/Users/"+os.getlogin()+"/opt/anaconda3/envs/drones/lib/python3.8/site-packages")  
+elif sys.platform == 'linux': # Ubuntu
+    sys.path.append("/home/"+getpass.getuser()+"/anaconda3/envs/drones/lib/python3.8/site-packages")  
 
 import rclpy
 import numpy as np
