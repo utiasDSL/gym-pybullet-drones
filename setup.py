cf54f50d7e5e3a089007c3dc55cb8c3d870b9650
Spencer Teetaert <spencerteetaert@gmail.com>
2022-06-13 15:17:06 -0400

    removed setup.py (legacy)

--- setup.py
+++
@@ -1,7 +0,0 @@
-import setuptools
-
-'''
-All imports and meta data is added to package from pyproject.toml. setup.py is 
-kept as it enables install in editable mode which is not supported in poetry builds.
-'''
-setuptools.setup()
