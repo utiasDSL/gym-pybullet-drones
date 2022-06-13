e506159118f7a0b39f03992b614434a0b5a9ce4d
Spencer Teetaert <spencerteetaert@gmail.com>
2022-06-13 11:29:12 -0400

    Fixed install of ray[rllib]. Removed meta data from setup.py and changed requirements in pyproject.toml. Removed build files from repo. To be built and published in github workflow

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
 import setuptools
 
-# read the contents of your README file
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
-setuptools.setup(name='gym_pybullet_drones',
-    version='1.0.0',
-    install_requires=[
-        'numpy',
-        'Pillow',
-        'matplotlib',
-        'cycler',
-        'gym',
-        'pybullet',
-        'stable_baselines3',
-        'ray[rllib]',
-        'tensorboard'
-        ],
-    packages=setuptools.find_packages(where="gym_pybullet_drones"),
-    package_dir={'':'gym_pybullet_drones'},
-    package_data={'gym_pybullet_drones': ['assets/*.*']},
-    long_description=long_description,
-    long_description_content_type='test/markdown',
-    license='MIT License\nCopyright (c) 2020 Jacopo Panerati',
-    entry_points = {
-        'console_scripts': ['downwash=examples.downwash:main'],
-    }
-)
+'''
+All imports and meta data is added to package from pyproject.toml. setup.py is 
+kept as it enables install in editable mode which is not supported in poetry builds.
+'''
+setuptools.setup()
