7d9a5b1b0f2d39d1c44e663f31b51be1214c4e05
paehal <81864283+paehal@users.noreply.github.com>
2023-03-13 23:00:26 +0900

    Update BaseAviary.py

--- gym_pybullet_drones/envs/BaseAviary.py
+++ gym_pybullet_drones/envs/BaseAviary.py
@@ -240,7 +240,7 @@ class BaseAviary(gym.Env):
         #### Start video recording #################################
         self._startVideoRecording()
         #### Return the initial observation ########################
-        #return self._computeObs() #ray2.2
+        #return self._computeObs() #ray1.9
         return self._computeObs(), {} #ray2.3
     
     ################################################################################
@@ -355,7 +355,7 @@ class BaseAviary(gym.Env):
         info = self._computeInfo()
         #### Advance the step counter ##############################
         self.step_counter = self.step_counter + (1 * self.AGGR_PHY_STEPS)
-        #return obs, reward, done, info #ray2.2
+        #return obs, reward, done, info #ray1.9
         terminated = done #ray2.3
         truncated = False #ray2.3
         return obs, reward, terminated, truncated, info #ray2.3
