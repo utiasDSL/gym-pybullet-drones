20261654f8806c021bd7c91dc9eac49d1bcbe74a
Jacopo Panerati <jacopo.panerati@utoronto.ca>
2023-04-16 11:25:19 +0400

    gymnasium, no baselines3

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,15 +1,15 @@
-def test_downwash():
-    from gym_pybullet_drones.examples.downwash import run
-    run(gui=False,plot=False,output_folder='tmp')
-
 def test_fly():
     from gym_pybullet_drones.examples.pid import run
-    run(gui=False,plot=False,output_folder='tmp')
-
-def test_learn():
-    from gym_pybullet_drones.examples.learning.learn import run
     run(gui=False,plot=False,output_folder='tmp')
 
 def test_velocity():
     from gym_pybullet_drones.examples.pid_velocity import run
     run(gui=False,plot=False,output_folder='tmp')
+
+def test_downwash():
+    from gym_pybullet_drones.examples.downwash import run
+    run(gui=False,plot=False,output_folder='tmp')
+
+# def test_learn():
+#     from gym_pybullet_drones.examples.learning.learn import run
+#     run(gui=False,plot=False,output_folder='tmp')
