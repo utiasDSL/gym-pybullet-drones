92a9c3c8d00edb24dbb22685bc36d80385d45e57
Jacopo Panerati <jacopo.panerati@utoronto.ca>
2023-10-11 15:14:56 +0400

    Added wip script to clone, compile, configure, run betaflight SITL

--- gym_pybullet_drones/assets/clone_bfs.sh
+++ gym_pybullet_drones/assets/clone_bfs.sh
@@ -4,3 +4,12 @@
 # 1. ..
 
 echo 'TBD'
+
+git clone https://github.com/betaflight/betaflight
+cd betaflight/
+# comment out line `delayMicroseconds_real(50); // max rate 20kHz` in ./src/main/main.c
+# edit ports in ./src/main/target/SITL/sitl.c, ./src/main/drivers/serial_tcp.c
+make arm_sdk_install 
+make TARGET=SITL 
+cp ~/gym-pybullet-drones/gym_pybullet_drones/assets/eeprom.bin ~/betaflight/
+./obj/main/betaflight_SITL.elf
