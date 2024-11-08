396bbb2f14f87e1fb8ecb90934fa3242ed5ced48
astik <41.astiksrivastava@gmail.com>
2024-11-08 16:54:47 +0530

    traj msg

--- ros2/ros2_and_pkg_setups.bash
+++
@@ -1,2 +0,0 @@
-source ~/ros2_foxy/ros2-osx/setup.bash      # change to the location of your ROS2 installation, if necessary
-source ./install/setup.bash                 # the relative path requires to source from folder /gym_pybullet_drones/ros2/
