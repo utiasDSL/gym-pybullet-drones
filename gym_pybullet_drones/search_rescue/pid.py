95c508e8e675afd0968ab4e346a449320278282e
Luiz <luiz@Luizs-MacBook-Pro.local>
2025-02-06 16:22:29 +0100

    Reestructured pid.py to use object oriented programming
    Description:
     - modified:   pid.py
        * Refactor of this file to improve code;
        * object prgramming approach so it is easier
        to maintain

--- gym_pybullet_drones/search_rescue/pid.py
+++ gym_pybullet_drones/search_rescue/pid.py
@@ -1,177 +1,146 @@
-"""Script demonstrating the joint use of simulation and control.
-
-The simulation is run by a `CtrlAviary` environment.
-The control is given by the PID implementation in `DSLPIDControl`.
-
-Example
--------
-In a terminal, run as:
-
-    $ python pid.py
-
-Notes
------
-The drones move, at different altitudes, along cicular trajectories 
-in the X-Y plane, around point (0, -.3).
-
-"""
 import os
 import time
 import argparse
-from datetime import datetime
-import pdb
-import math
-import random
 import numpy as np
 import pybullet as p
-import matplotlib.pyplot as plt
-
+from datetime import datetime
 from gym_pybullet_drones.utils.enums import DroneModel, Physics
 from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
 from gym_pybullet_drones.control.DSLPIDControl import DSLPIDControl
 from gym_pybullet_drones.utils.Logger import Logger
 from gym_pybullet_drones.utils.utils import sync, str2bool
 
-DEFAULT_DRONES = DroneModel("cf2x")
-DEFAULT_NUM_DRONES = 3
-DEFAULT_PHYSICS = Physics("pyb")
-DEFAULT_GUI = True
-DEFAULT_RECORD_VISION = False
-DEFAULT_PLOT = False
-DEFAULT_USER_DEBUG_GUI = False
-DEFAULT_OBSTACLES = True
-DEFAULT_SIMULATION_FREQ_HZ = 240
-DEFAULT_CONTROL_FREQ_HZ = 48
-DEFAULT_DURATION_SEC = 9999999
-DEFAULT_OUTPUT_FOLDER = 'results'
-DEFAULT_COLAB = False
+class DroneSimulation:
+    def __init__(self, **kwargs):
+        self.drone_model = kwargs.get("drone", DroneModel("cf2x"))
+        self.num_drones = kwargs.get("num_drones", 5)
+        self.physics = kwargs.get("physics", Physics("pyb_gnd_drag_dw"))
+        self.gui = kwargs.get("gui", True)
+        self.record_video = kwargs.get("record_video", False)
+        self.plot = kwargs.get("plot", False)
+        self.user_debug_gui = kwargs.get("user_debug_gui", False)
+        self.obstacles = kwargs.get("obstacles", True)
+        self.simulation_freq_hz = kwargs.get("simulation_freq_hz", 240)
+        self.control_freq_hz = kwargs.get("control_freq_hz", 48)
+        self.duration_sec = kwargs.get("duration_sec", 9999999)
+        self.output_folder = kwargs.get("output_folder", "results")
+        self.colab = kwargs.get("colab", False)
+        self.env = None
+        self.logger = None
+        self.controllers = []
+        self.action = np.zeros((self.num_drones, 4))
+        self.init_positions()
+        self.init_trajectory2() # or init_trajectory2
 
-def run(
-        drone=DEFAULT_DRONES,
-        num_drones=DEFAULT_NUM_DRONES,
-        physics=DEFAULT_PHYSICS,
-        gui=DEFAULT_GUI,
-        record_video=DEFAULT_RECORD_VISION,
-        plot=DEFAULT_PLOT,
-        user_debug_gui=DEFAULT_USER_DEBUG_GUI,
-        obstacles=DEFAULT_OBSTACLES,
-        simulation_freq_hz=DEFAULT_SIMULATION_FREQ_HZ,
-        control_freq_hz=DEFAULT_CONTROL_FREQ_HZ,
-        duration_sec=DEFAULT_DURATION_SEC,
-        output_folder=DEFAULT_OUTPUT_FOLDER,
-        colab=DEFAULT_COLAB
-        ):
-    #### Initialize the simulation #############################
-    H = .1
-    H_STEP = .05
-    R = .3
-    INIT_XYZS = np.array([[R*np.cos((i/6)*2*np.pi+np.pi/2), R*np.sin((i/6)*2*np.pi+np.pi/2)-R, H+i*H_STEP] for i in range(num_drones)])
-    INIT_RPYS = np.array([[0, 0,  i * (np.pi/2)/num_drones] for i in range(num_drones)])
+    def init_positions(self):
+        H = 0.5
+        H_STEP = 0.05
+        R = 0.3
+        self.init_xyzs = np.array([
+            [R * np.cos((i / 6) * 2 * np.pi + np.pi / 2), R * np.sin((i / 6) * 2 * np.pi + np.pi / 2) - R, H + i * H_STEP]
+            for i in range(self.num_drones)
+        ])
+        self.init_rpys = np.array([[0, 0, i * (np.pi / 2) / self.num_drones] for i in range(self.num_drones)])
 
-    #### Initialize a circular trajectory ######################
-    PERIOD = 10
-    NUM_WP = control_freq_hz*PERIOD
-    TARGET_POS = np.zeros((NUM_WP,3))
-    for i in range(NUM_WP):
-        TARGET_POS[i, :] = R*np.cos((i/NUM_WP)*(2*np.pi)+np.pi/2)+INIT_XYZS[0, 0], R*np.sin((i/NUM_WP)*(2*np.pi)+np.pi/2)-R+INIT_XYZS[0, 1], 0
-    wp_counters = np.array([int((i*NUM_WP/6)%NUM_WP) for i in range(num_drones)])
+    def init_trajectory2(self):
+            PERIOD = 14
+            NUM_WP = self.control_freq_hz * PERIOD
+            self.target_pos = np.zeros((NUM_WP, 3))
+            RADIUS = 1
+            
+            # Alternative trajectory (figure-eight pattern)
+            for i in range(NUM_WP):
+                theta = (i / NUM_WP) * (2 * np.pi)
+                x = RADIUS * np.sin(2 * theta)
+                y = RADIUS * np.sin(theta)
+                self.target_pos[i, :] = [x, y, 0]
+            
+            self.wp_counters = np.array([int((i * NUM_WP / 6) % NUM_WP) for i in range(self.num_drones)])
+        
 
-    #### Create the environment ################################
-    env = CtrlAviary(drone_model=drone,
-                        num_drones=num_drones,
-                        initial_xyzs=INIT_XYZS,
-                        initial_rpys=INIT_RPYS,
-                        physics=physics,
-                        neighbourhood_radius=10,
-                        pyb_freq=simulation_freq_hz,
-                        ctrl_freq=control_freq_hz,
-                        gui=gui,
-                        record=record_video,
-                        obstacles=obstacles,
-                        user_debug_gui=user_debug_gui
-                        )
+    def init_trajectory(self):
+        PERIOD = 10
+        NUM_WP = self.control_freq_hz * PERIOD
+        self.target_pos = np.zeros((NUM_WP, 3))
+        for i in range(NUM_WP):
+            self.target_pos[i, :] = (
+                self.init_xyzs[0, 0] + 0.3 * np.cos((i / NUM_WP) * (2 * np.pi) + np.pi / 2),
+                self.init_xyzs[0, 1] + 0.3 * np.sin((i / NUM_WP) * (2 * np.pi) + np.pi / 2) - 0.3,
+                self.init_xyzs[0, 2],  # Use the initial height of the first drone
+            )
+        self.wp_counters = np.array([int((i * NUM_WP / 6) % NUM_WP) for i in range(self.num_drones)])
 
-    #### Obtain the PyBullet Client ID from the environment ####
-    PYB_CLIENT = env.getPyBulletClient()
+    def setup_environment(self):
+        self.env = CtrlAviary(
+            drone_model=self.drone_model,
+            num_drones=self.num_drones,
+            initial_xyzs=self.init_xyzs,
+            initial_rpys=self.init_rpys,
+            physics=self.physics,
+            neighbourhood_radius=10,
+            pyb_freq=self.simulation_freq_hz,
+            ctrl_freq=self.control_freq_hz,
+            gui=self.gui,
+            record=self.record_video,
+            obstacles=self.obstacles,
+            user_debug_gui=self.user_debug_gui,
+        )
+        self.logger = Logger(
+            logging_freq_hz=self.control_freq_hz,
+            num_drones=self.num_drones,
+            output_folder=self.output_folder,
+            colab=self.colab,
+        )
+        self.controllers = [DSLPIDControl(drone_model=self.drone_model) for _ in range(self.num_drones)]
 
-    #### Initialize the logger #################################
-    logger = Logger(logging_freq_hz=control_freq_hz,
-                    num_drones=num_drones,
-                    output_folder=output_folder,
-                    colab=colab
-                    )
+    def run_simulation(self):
+        self.setup_environment()
+        start_time = time.time()
+        for i in range(0, int(self.duration_sec * self.env.CTRL_FREQ)):
+            obs, _, _, _, _ = self.env.step(self.action)
+            self.update_controls(obs)
+            self.log_data(obs, i)
+            if self.gui:
+                sync(i, start_time, self.env.CTRL_TIMESTEP)
+        self.env.close()
+        if self.plot:
+            self.logger.plot()
 
-    #### Initialize the controllers ############################
-    if drone in [DroneModel.CF2X, DroneModel.CF2P]:
-        ctrl = [DSLPIDControl(drone_model=drone) for i in range(num_drones)]
+    def update_controls(self, obs):
+        for j in range(self.num_drones):
+            self.action[j, :], _, _ = self.controllers[j].computeControlFromState(
+                control_timestep=self.env.CTRL_TIMESTEP,
+                state=obs[j],
+                target_pos=np.hstack([self.target_pos[self.wp_counters[j], 0:2], self.init_xyzs[j, 2]]),
+                target_rpy=self.init_rpys[j, :],
+            )
+            self.wp_counters[j] = (self.wp_counters[j] + 1) % len(self.target_pos)
 
-    #### Run the simulation ####################################
-    action = np.zeros((num_drones,4))
-    START = time.time()
-    for i in range(0, int(duration_sec*env.CTRL_FREQ)):
-
-        #### Make it rain rubber ducks #############################
-        # if i/env.SIM_FREQ>5 and i%10==0 and i/env.SIM_FREQ<10: p.loadURDF("duck_vhacd.urdf", [0+random.gauss(0, 0.3),-0.5+random.gauss(0, 0.3),3], p.getQuaternionFromEuler([random.randint(0,360),random.randint(0,360),random.randint(0,360)]), physicsClientId=PYB_CLIENT)
-
-        #### Step the simulation ###################################
-        obs, reward, terminated, truncated, info = env.step(action)
-
-        #### Compute control for the current way point #############
-        for j in range(num_drones):
-            action[j, :], _, _ = ctrl[j].computeControlFromState(control_timestep=env.CTRL_TIMESTEP,
-                                                                    state=obs[j],
-                                                                    target_pos=np.hstack([TARGET_POS[wp_counters[j], 0:2], INIT_XYZS[j, 2]]),
-                                                                    # target_pos=INIT_XYZS[j, :] + TARGET_POS[wp_counters[j], :],
-                                                                    target_rpy=INIT_RPYS[j, :]
-                                                                    )
-
-        #### Go to the next way point and loop #####################
-        for j in range(num_drones):
-            wp_counters[j] = wp_counters[j] + 1 if wp_counters[j] < (NUM_WP-1) else 0
-
-        #### Log the simulation ####################################
-        for j in range(num_drones):
-            logger.log(drone=j,
-                       timestamp=i/env.CTRL_FREQ,
-                       state=obs[j],
-                       control=np.hstack([TARGET_POS[wp_counters[j], 0:2], INIT_XYZS[j, 2], INIT_RPYS[j, :], np.zeros(6)])
-                       # control=np.hstack([INIT_XYZS[j, :]+TARGET_POS[wp_counters[j], :], INIT_RPYS[j, :], np.zeros(6)])
-                       )
-
-        #### Printout ##############################################
-        g#env.render()
-
-        #### Sync the simulation ###################################
-        if gui:
-            sync(i, START, env.CTRL_TIMESTEP)
-
-    #### Close the environment #################################
-    env.close()
-
-    #### Save the simulation results ###########################
-    #logger.save()
-    #logger.save_as_csv("pid") # Optional CSV save
-
-    #### Plot the simulation results ###########################
-    if plot:
-        logger.plot()
+    def log_data(self, obs, step):
+        for j in range(self.num_drones):
+            self.logger.log(
+                drone=j,
+                timestamp=step / self.env.CTRL_FREQ,
+                state=obs[j],
+                control=np.hstack([self.target_pos[self.wp_counters[j], 0:2], self.init_xyzs[j, 2], self.init_rpys[j, :], np.zeros(6)]),
+            )
 
 if __name__ == "__main__":
-    #### Define and parse (optional) arguments for the script ##
-    parser = argparse.ArgumentParser(description='Helix flight script using CtrlAviary and DSLPIDControl')
-    parser.add_argument('--drone',              default=DEFAULT_DRONES,     type=DroneModel,    help='Drone model (default: CF2X)', metavar='', choices=DroneModel)
-    parser.add_argument('--num_drones',         default=DEFAULT_NUM_DRONES,          type=int,           help='Number of drones (default: 3)', metavar='')
-    parser.add_argument('--physics',            default=DEFAULT_PHYSICS,      type=Physics,       help='Physics updates (default: PYB)', metavar='', choices=Physics)
-    parser.add_argument('--gui',                default=DEFAULT_GUI,       type=str2bool,      help='Whether to use PyBullet GUI (default: True)', metavar='')
-    parser.add_argument('--record_video',       default=DEFAULT_RECORD_VISION,      type=str2bool,      help='Whether to record a video (default: False)', metavar='')
-    parser.add_argument('--plot',               default=DEFAULT_PLOT,       type=str2bool,      help='Whether to plot the simulation results (default: True)', metavar='')
-    parser.add_argument('--user_debug_gui',     default=DEFAULT_USER_DEBUG_GUI,      type=str2bool,      help='Whether to add debug lines and parameters to the GUI (default: False)', metavar='')
-    parser.add_argument('--obstacles',          default=DEFAULT_OBSTACLES,       type=str2bool,      help='Whether to add obstacles to the environment (default: True)', metavar='')
-    parser.add_argument('--simulation_freq_hz', default=DEFAULT_SIMULATION_FREQ_HZ,        type=int,           help='Simulation frequency in Hz (default: 240)', metavar='')
-    parser.add_argument('--control_freq_hz',    default=DEFAULT_CONTROL_FREQ_HZ,         type=int,           help='Control frequency in Hz (default: 48)', metavar='')
-    parser.add_argument('--duration_sec',       default=DEFAULT_DURATION_SEC,         type=int,           help='Duration of the simulation in seconds (default: 5)', metavar='')
-    parser.add_argument('--output_folder',     default=DEFAULT_OUTPUT_FOLDER, type=str,           help='Folder where to save logs (default: "results")', metavar='')
-    parser.add_argument('--colab',              default=DEFAULT_COLAB, type=bool,           help='Whether example is being run by a notebook (default: "False")', metavar='')
-    ARGS = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Helix flight simulation using CtrlAviary and DSLPIDControl")
+    parser.add_argument("--drone", default=DroneModel("cf2x"), type=DroneModel, choices=DroneModel)
+    parser.add_argument("--num_drones", default=4, type=int)
+    parser.add_argument("--physics", default=Physics("pyb_gnd_drag_dw"), type=Physics, choices=Physics)
+    parser.add_argument("--gui", default=True, type=str2bool)
+    parser.add_argument("--record_video", default=False, type=str2bool)
+    parser.add_argument("--plot", default=False, type=str2bool)
+    parser.add_argument("--user_debug_gui", default=False, type=str2bool)
+    parser.add_argument("--obstacles", default=True, type=str2bool)
+    parser.add_argument("--simulation_freq_hz", default=240, type=int)
+    parser.add_argument("--control_freq_hz", default=48, type=int)
+    parser.add_argument("--duration_sec", default=9999999, type=int)
+    parser.add_argument("--output_folder", default="results", type=str)
+    parser.add_argument("--colab", default=False, type=bool)
+    args = parser.parse_args()
 
-    run(**vars(ARGS))
+    sim = DroneSimulation(**vars(args))
+    sim.run_simulation()
